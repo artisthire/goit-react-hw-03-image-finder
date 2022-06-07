f80dbdd810a40e22032a0d4c22ad6a7f9692a390
Andrey <artist_hire@ukr.net>
2022-06-07 13:40:49 +0300

    Start

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { Wrapper, Container } from './App.styled';
+
+const App = () => {
+  return (
+    <Wrapper>
+      <Container>React homework template</Container>
+    </Wrapper>
+  );
+};
+
+// for test 2
+
+export default App;
